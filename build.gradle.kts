6d0a131bed4211fe4b6ee79d16e5da451cfcf2c9
b4tchkn <baaaakkrad@gmail.com>
2021-08-10 23:35:16 +0900

    feat: add files

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("multiplatform") version "1.5.21"
+    id("org.jetbrains.compose") version "0.5.0-build270"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
