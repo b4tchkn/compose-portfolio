5334ead7db53f57128bcb05b180b028cb58de3ab
b4tchkn <baaaakkrad@gmail.com>
2021-08-18 22:43:10 +0900

    feat: add icopn

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Footer
+import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 import style.AppStyleSheet
 import style.AppStyleSheet.footer
+import style.AppStyleSheet.icon
+import style.AppStyleSheet.space
 import style.TextStyleSheet.caption
 import style.TextStyleSheet.large
 import style.TextStyleSheet.medium
@@ -13,9 +16,11 @@ fun main() {
     renderComposable(rootElementId = "root") {
         Style(AppStyleSheet)
         Div({ classes(AppStyleSheet.centerContainer) }) {
+            Img(src = "icon.png", attrs = {classes(icon)})
             Div(attrs = { classes(large) }) {
                 Text("Kakeru Nakabachi / batch")
             }
+            Div(attrs = { classes(space) }) { }
             Div(attrs = { classes(medium) }) {
                 Text("Mobile App Developer Android, Flutter")
             }
--- src/main/kotlin/style/AppStyleSheet.kt
+++ src/main/kotlin/style/AppStyleSheet.kt
@@ -7,12 +7,18 @@ import org.jetbrains.compose.web.css.JustifyContent
 import org.jetbrains.compose.web.css.Position
 import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.alignItems
+import org.jetbrains.compose.web.css.background
+import org.jetbrains.compose.web.css.borderRadius
 import org.jetbrains.compose.web.css.bottom
+import org.jetbrains.compose.web.css.color
 import org.jetbrains.compose.web.css.display
 import org.jetbrains.compose.web.css.flexDirection
 import org.jetbrains.compose.web.css.height
 import org.jetbrains.compose.web.css.justifyContent
+import org.jetbrains.compose.web.css.marginBottom
+import org.jetbrains.compose.web.css.maxWidth
 import org.jetbrains.compose.web.css.paddingBottom
+import org.jetbrains.compose.web.css.pc
 import org.jetbrains.compose.web.css.position
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.vh
@@ -27,6 +33,7 @@ object AppStyleSheet : StyleSheet() {
         alignItems(AlignItems.Center)
         width(100.vw)
         height(100.vh)
+        background("#E8E8E8")
     }
 
     val footer by style {
@@ -37,5 +44,15 @@ object AppStyleSheet : StyleSheet() {
         paddingBottom(16.px)
         alignItems(AlignItems.Center)
         width(100.vw)
+    }
+
+    val icon by style {
+        maxWidth(200.px)
+        borderRadius(50.pc)
+        marginBottom(16.px)
+    }
+
+    val space by style {
+        height(8.px)
     }
 }
