c05aa005d8ffff7a6f12a970893cadab6ad703c5
b4tchkn <baaaakkrad@gmail.com>
2021-08-18 23:10:24 +0900

    feat: add SocialIconButton

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.attributes.ATarget
+import org.jetbrains.compose.web.attributes.target
 import org.jetbrains.compose.web.css.Style
+import org.jetbrains.compose.web.css.marginTop
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.A
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Footer
 import org.jetbrains.compose.web.dom.Img
@@ -7,7 +13,7 @@ import org.jetbrains.compose.web.renderComposable
 import style.AppStyleSheet
 import style.AppStyleSheet.footer
 import style.AppStyleSheet.icon
-import style.AppStyleSheet.space
+import style.AppStyleSheet.socialIconButton
 import style.TextStyleSheet.caption
 import style.TextStyleSheet.large
 import style.TextStyleSheet.medium
@@ -16,17 +22,43 @@ fun main() {
     renderComposable(rootElementId = "root") {
         Style(AppStyleSheet)
         Div({ classes(AppStyleSheet.centerContainer) }) {
-            Img(src = "icon.png", attrs = {classes(icon)})
+            Img(src = "icon.png", attrs = { classes(icon) })
             Div(attrs = { classes(large) }) {
                 Text("Kakeru Nakabachi / batch")
             }
-            Div(attrs = { classes(space) }) { }
-            Div(attrs = { classes(medium) }) {
+            Div(attrs = {
+                style {
+                    marginTop(16.px)
+                }
+                classes(medium)
+            }) {
                 Text("Mobile App Developer Android, Flutter")
+            }
+            Div(attrs = {
+                style {
+                    marginTop(8.px)
+                }
+            }) {
+                SocialIconButton()
+                SocialIconButton()
+                SocialIconButton()
+                SocialIconButton()
+                SocialIconButton()
+                SocialIconButton()
             }
         }
         Footer(attrs = { classes(footer, caption) }) {
             Text("© 2021 · Powered by Jetpack Compose")
         }
+    }
+}
+
+@Composable
+private fun SocialIconButton() {
+    A(attrs = {
+        classes(socialIconButton)
+        target(ATarget.Blank)
+    }, href = "https://compose-web.ui.pages.jetbrains.team/") {
+        Img(src = "favicon-32x32.png") {}
     }
 }
--- src/main/kotlin/style/AppStyleSheet.kt
+++ src/main/kotlin/style/AppStyleSheet.kt
@@ -1,6 +1,7 @@
 package style
 
 import org.jetbrains.compose.web.css.AlignItems
+import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.FlexDirection
 import org.jetbrains.compose.web.css.JustifyContent
@@ -8,19 +9,26 @@ import org.jetbrains.compose.web.css.Position
 import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.alignItems
 import org.jetbrains.compose.web.css.background
+import org.jetbrains.compose.web.css.backgroundColor
 import org.jetbrains.compose.web.css.borderRadius
 import org.jetbrains.compose.web.css.bottom
-import org.jetbrains.compose.web.css.color
 import org.jetbrains.compose.web.css.display
 import org.jetbrains.compose.web.css.flexDirection
 import org.jetbrains.compose.web.css.height
 import org.jetbrains.compose.web.css.justifyContent
 import org.jetbrains.compose.web.css.marginBottom
+import org.jetbrains.compose.web.css.marginLeft
+import org.jetbrains.compose.web.css.marginRight
 import org.jetbrains.compose.web.css.maxWidth
+import org.jetbrains.compose.web.css.media
+import org.jetbrains.compose.web.css.mediaMaxWidth
+import org.jetbrains.compose.web.css.padding
 import org.jetbrains.compose.web.css.paddingBottom
 import org.jetbrains.compose.web.css.pc
 import org.jetbrains.compose.web.css.position
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.rgba
+import org.jetbrains.compose.web.css.selectors.hover
 import org.jetbrains.compose.web.css.vh
 import org.jetbrains.compose.web.css.vw
 import org.jetbrains.compose.web.css.width
@@ -33,7 +41,7 @@ object AppStyleSheet : StyleSheet() {
         alignItems(AlignItems.Center)
         width(100.vw)
         height(100.vh)
-        background("#E8E8E8")
+        background("#F1F1F1")
     }
 
     val footer by style {
@@ -47,12 +55,28 @@ object AppStyleSheet : StyleSheet() {
     }
 
     val icon by style {
-        maxWidth(200.px)
+        maxWidth(160.px)
         borderRadius(50.pc)
         marginBottom(16.px)
     }
 
-    val space by style {
-        height(8.px)
+    val socialIconButton by style {
+        marginRight(8.px)
+        marginLeft(8.px)
+        padding(12.px)
+        backgroundColor(Color("transparent"))
+        display(DisplayStyle.LegacyInlineFlex)
+
+        hover(self) style {
+            backgroundColor(rgba(0, 0, 0, 0.1))
+            borderRadius(50.pc)
+        }
+
+        media(mediaMaxWidth(640.px)) {
+            self style {
+                marginRight(8.px)
+                marginLeft(8.px)
+            }
+        }
     }
 }
